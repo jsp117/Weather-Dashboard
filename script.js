51fd320f47e11e4a1bdd13ef9770340eef7052f8
jsp <jonsp14@gmail.com>
2020-09-28 18:41:34 -0700

    add event handler for created buttons, add error checking, remove item from array if city doesn't exist, remove button if city doesn't exist

--- script.js
+++ script.js
@@ -1,87 +1,153 @@
+// waits for page to load before starting
+// $(document).ready(function () {
 
 var container = [];
+var add = true;
+var save = false;
 
+function search(x) {
 
-function searchWeather(x) {
-    $.ajax({
-      type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + x + "&appid=ebdcfef8b5f11a300f888ec06cffdf1c&units=imperial",
-      dataType: "json",
-      // error handling
-      error: function(e){
-          alert("Error-Connection");
-      }
+  $.ajax({
+    type: "GET",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + x + "&appid=ebdcfef8b5f11a300f888ec06cffdf1c&units=imperial",
+    dataType: "json",
+    // error handling
+    error: function (e) {
+      save = false;
+      alert("Error: City does not exist - or connection issues");
+      exist(x);
+      return save;
+    }
     // runs whenever call goes through
-    }).then(function(response){
-        console.log(response);
-        console.log(response.name)
-        $("#cityName").text("City: " + response.name);
-        var temp = $("<p>");
-        temp.text("Temperature: " + response.main.temp + " F");
-        $("#cityInfo").append(temp);
-        var humidity = $("<p>");
-        humidity.text("humidity: " + response.main.humidity + " %");
-        $("#cityInfo").append(humidity);
-        var wind = $("<p>");
-        wind.text("Temperature: " + response.wind.speed + " MPH");
-        $("#cityInfo").append(wind);
-        var uv = $("<p>");
-        uv.text("UV");
-        $("#cityInfo").append(uv);
-        
-        addCity(response.name);
-    })
-  }
-
-//   var searchValue = "Austin";
-//   searchWeather(searchValue);
-
-  $("#search").on("click", function(event){
-    event.preventDefault();
-    var input = $("#city").val();
-    console.log("input: " + input);
-    searchWeather(input);
+  }).then(function (response) {
+    save = true;
+    console.log(response);
+    console.log(response.name)
+    $("#cityName").text("City: " + response.name);
+    var temp = $("<p>");
+    temp.text("Temperature: " + response.main.temp + " F");
+    $("#cityInfo").append(temp);
+    var humidity = $("<p>");
+    humidity.text("humidity: " + response.main.humidity + " %");
+    $("#cityInfo").append(humidity);
+    var wind = $("<p>");
+    wind.text("Temperature: " + response.wind.speed + " MPH");
+    $("#cityInfo").append(wind);
+    var uv = $("<p>");
+    uv.text("UV");
+    $("#cityInfo").append(uv);
+    return save;
   });
+}
 
-  function addCity(x){
-      var newLi = $("<li>");
-      // newLi.text(x);
-      $("#cities").append(newLi);
-      var button = $("<button>");
-      button.text(x);
-      button.addClass("btn btn-secondary cityButton mb-2");
-      newLi.append(button);
-      container.push(x);
-      console.log(container);
-      for(var i = 0; i < container.length; i++){
-      localStorage.setItem(i, container[i]);
+function exist(val) {
+  // remove item from array
+  var popped = container.pop(val);
+  console.log("popped value: " + popped);
+  console.log("new container: " + container);
+  // remove last button created
+  $("#cities li").last().remove();
+}
+
+$("#search").on("click", function (event, x) {
+  // clear previous info
+  $("#cityInfo").empty();
+  event.preventDefault();
+  var input = $("#city").val();
+  console.log("input: " + input);
+  // compare new input to container
+  add = true;
+  for (var i = 0; i < container.length; i++) {
+    if (input === container[i]) {
+      add = false;
+      console.log("already exists");
+      alert("Please enter a different city");
+      $("#city").val("");
+      return;
     }
-    localStorage.setItem("Number", container.length);
   }
-
-  $(".cityButton").on("click", function(event){
+  if (input === "" || add === false || $.type(input) != "string") {
     event.preventDefault();
-    var btnVal = this.text;
-    console.log(btnVal);
+    alert("You must enter an existing city that you have not already entered!");
+  } else {
+    container.push(input);
+    console.log("new item added: " + container);
+    search(input);
+    
+      addCity(input);
+    
+    $("#city").val("");
+  }
+});
 
-  });
+function addCity(x) {
+  console.log(container);
+  var newLi = $("<li>");
+  $("#cities").append(newLi);
+  var button = $("<button>");
+  button.text(x);
+  button.addClass("btn btn-secondary cityButton mb-2");
+  button.attr("type", "button");
+  // button.attr("id", container.length);
+  // button.attr("onClick", "grabText()");
+  newLi.append(button);
 
-  function pageOpen(){
-    var num = localStorage.getItem("Number");
-    for(var i = 0; i < num; i++){
-      container.push(localStorage.getItem(i));
-    }
 
-    for(var i = 0; i < container.length; i++){
-      var li = $("<li>");
-      // newLi.text(x);
-      $("#cities").append(li);
-      var ogButton = $("<button>");
-      ogButton.text(container[i]);
-      ogButton.addClass("btn btn-secondary cityButton mb-2");
-      li.append(ogButton);
-    }
-    console.log(container);
+
+  console.log(container);
+  for (var i = 0; i < container.length; i++) {
+    localStorage.setItem(i, container[i]);
+  }
+  localStorage.setItem("Number", container.length);
+
+}
+
+// button first try
+$("#cities").on("click", "button", function (event) {
+  $("#cityInfo").empty();
+  console.log("hello");
+  event.preventDefault();
+  var btnText = $(this).text();
+  console.log("btn text: " + btnText);
+  search(btnText);
+
+});
+
+// button second try
+// function grabText(event) {
+//   // event.preventDefault();
+//   var newTest = $(this).text();
+//   console.log("new Test: " , newTest);
+// }
+
+function pageOpen() {
+  var num = localStorage.getItem("Number");
+  for (var i = 0; i < num; i++) {
+    container.push(localStorage.getItem(i));
   }
 
-  pageOpen();
+  for (var i = 0; i < container.length; i++) {
+    var li = $("<li>");
+    $("#cities").append(li);
+    var ogButton = $("<button>");
+    ogButton.text(container[i]);
+    ogButton.addClass("btn btn-secondary cityButton mb-2");
+    ogButton.attr("type", "button");
+    // ogButton.attr("id", i + 1);
+    // ogButton.attr("onClick", "grabText()");
+    li.append(ogButton);
+    // ogButton.addEventListener("click", function(event) {
+    //   event.preventDefault();
+    //   var buttonTxt = $(this).text();
+    //   console.log("ogButton text: " + buttonTxt);
+
+    // });
+
+  }
+  // console.log(container);
+}
+
+pageOpen();
+
+
+// });
