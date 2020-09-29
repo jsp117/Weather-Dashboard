67fde1853977a62083063c9c4a34fa9bf288c1cc
jsp <jonsp14@gmail.com>
2020-09-28 20:46:25 -0700

    add icons to 5 day forecast

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // waits for page to load before starting
-// $(document).ready(function () {
+$(document).ready(function () {
 
 var container = [];
 var add = true;
@@ -23,7 +23,7 @@ function search(x) {
     $(".forecast").attr("style", "display: inline-block");
     save = true;
     console.log(response);
-    console.log(response.name)
+    console.log(response.name);
     $("#cityName").text("City: " + response.name);
     var temp = $("<p>");
     temp.text("Temperature: " + response.main.temp + " F");
@@ -42,16 +42,16 @@ function search(x) {
 }
 
 function uvDisplay(x, y) {
-  console.log("WORKING");
+  // console.log("WORKING");
   $.ajax({
     type: "GET",
     url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + x + "&lon=" + y + "&appid=ebdcfef8b5f11a300f888ec06cffdf1c",
     dataType: "json",
     // runs whenever call goes through
   }).then(function (response) {
-    console.log(response);
+    // console.log(response);
     var uvVal = parseFloat(response.value);
-    console.log("uv: " + uvVal);
+    // console.log("uv: " + uvVal);
     var uv = $("<p>");
     uv.attr("class", "p-1 col-lg-3");
     uv.text("UV index: " + uvVal);
@@ -81,54 +81,69 @@ function fiveDay(name) {
     url: "http://api.openweathermap.org/data/2.5/forecast?q=" + name + "&appid=ebdcfef8b5f11a300f888ec06cffdf1c&units=imperial",
     dataType: "json",
   }).then(function (response) {
-    console.log("five day: ", response);
-    var date = $("<p>");
+    // console.log("five day: ", response);
+    var date = $("<h5>");
     var temp = $("<p>");
     var humidity = $("<p>");
+    var img = $("<img>");
+    img.attr("src", "http://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + ".png");
     date.text(response.list[3].dt_txt);
     temp.text("Temp: " + response.list[3].main.temp_max + " F");
     humidity.text("Humidity: " + response.list[3].main.humidity + " %");
     $("#1").append(date);
+    $("#1").append(img);
     $("#1").append(temp);
     $("#1").append(humidity);
 
-    var date2 = $("<p>");
+    var date2 = $("<h5>");
     var temp2 = $("<p>");
     var humidity2 = $("<p>");
+    var img2 = $("<img>");
+    img2.attr("src", "http://openweathermap.org/img/wn/" + response.list[11].weather[0].icon + ".png");
     date2.text(response.list[11].dt_txt);
     temp2.text("Temp: " + response.list[11].main.temp_max + " F");
     humidity2.text("Humidity: " + response.list[11].main.humidity + " %");
     $("#2").append(date2);
+    $("#2").append(img2);
     $("#2").append(temp2);
     $("#2").append(humidity2);
 
-    var date3 = $("<p>");
+    var date3 = $("<h5>");
     var temp3 = $("<p>");
     var humidity3 = $("<p>");
+    var img3 = $("<img>");
+    img3.attr("src", "http://openweathermap.org/img/wn/" + response.list[19].weather[0].icon + ".png");
     date3.text(response.list[19].dt_txt);
     temp3.text("Temp: " + response.list[19].main.temp_max + " F");
     humidity3.text("Humidity: " + response.list[19].main.humidity + " %");
     $("#3").append(date3);
+    $("#3").append(img3);
     $("#3").append(temp3);
     $("#3").append(humidity3);
 
-    var date4 = $("<p>");
+    var date4 = $("<h5>");
     var temp4 = $("<p>");
     var humidity4 = $("<p>");
+    var img4 = $("<img>");
+    img4.attr("src", "http://openweathermap.org/img/wn/" + response.list[27].weather[0].icon + ".png");
     date4.text(response.list[27].dt_txt);
     temp4.text("Temp: " + response.list[27].main.temp_max + " F");
     humidity4.text("Humidity: " + response.list[27].main.humidity + " %");
     $("#4").append(date4);
+    $("#4").append(img4);
     $("#4").append(temp4);
     $("#4").append(humidity4);
 
-    var date5 = $("<p>");
+    var date5 = $("<h5>");
     var temp5 = $("<p>");
     var humidity5 = $("<p>");
+    var img5 = $("<img>");
+    img5.attr("src", "http://openweathermap.org/img/wn/" + response.list[35].weather[0].icon + ".png");
     date5.text(response.list[35].dt_txt);
     temp5.text("Temp: " + response.list[35].main.temp_max + " F");
     humidity5.text("Humidity: " + response.list[35].main.humidity + " %");
     $("#5").append(date5);
+    $("#5").append(img5);
     $("#5").append(temp5);
     $("#5").append(humidity5);
   });
@@ -139,8 +154,8 @@ function fiveDay(name) {
 function exist(val, toSave) {
   // remove item from array
   var popped = container.pop(val);
-  console.log("popped value: " + popped);
-  console.log("new container: " + container);
+  // console.log("popped value: " + popped);
+  // console.log("new container: " + container);
   // remove last button created
   $("#cities li").last().remove();
 }
@@ -151,13 +166,13 @@ $("#search").on("click", function (event, x) {
   $(".forecast").empty();
   event.preventDefault();
   var input = $("#city").val();
-  console.log("input: " + input);
+  // console.log("input: " + input);
   // compare new input to container
   add = true;
   for (var i = 0; i < container.length; i++) {
     if (input === container[i]) {
       add = false;
-      console.log("already exists");
+      // console.log("already exists");
       alert("Please enter a different city");
       $("#city").val("");
       return;
@@ -168,7 +183,7 @@ $("#search").on("click", function (event, x) {
     alert("You must enter an existing city that you have not already entered!");
   } else {
     container.push(input);
-    console.log("new item added: " + container);
+    // console.log("new item added: " + container);
     search(input);
 
     addCity(input);
@@ -178,20 +193,18 @@ $("#search").on("click", function (event, x) {
 });
 
 function addCity(x) {
-  console.log(container);
+  // console.log(container);
   var newLi = $("<li>");
   $("#cities").append(newLi);
   var button = $("<button>");
   button.text(x);
   button.addClass("btn btn-secondary cityButton mb-2");
   button.attr("type", "button");
-  // button.attr("id", container.length);
-  // button.attr("onClick", "grabText()");
   newLi.append(button);
 
 
 
-  console.log(container);
+  // console.log(container);
   for (var i = 0; i < container.length; i++) {
     localStorage.setItem(i, container[i]);
   }
@@ -201,11 +214,11 @@ function addCity(x) {
 
 // event handlers for created buttons
 $("#cities").on("click", "button", function (event) {
-  $("#cityInfo").empty();
-  console.log("hello");
   event.preventDefault();
+  $("#cityInfo").empty();
+  // console.log("hello");
   var btnText = $(this).text();
-  console.log("btn text: " + btnText);
+  // console.log("btn text: " + btnText);
   search(btnText);
 
 });
@@ -231,21 +244,15 @@ function pageOpen() {
     ogButton.text(container[i]);
     ogButton.addClass("btn btn-secondary cityButton mb-2");
     ogButton.attr("type", "button");
-    // ogButton.attr("id", i + 1);
-    // ogButton.attr("onClick", "grabText()");
+    
     li.append(ogButton);
-    // ogButton.addEventListener("click", function(event) {
-    //   event.preventDefault();
-    //   var buttonTxt = $(this).text();
-    //   console.log("ogButton text: " + buttonTxt);
-
-    // });
+   
 
   }
-  // console.log(container);
+  
 }
 
 pageOpen();
 
 
-// });
+});
