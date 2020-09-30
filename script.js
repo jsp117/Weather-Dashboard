75b29c5d34525858d67e5359389fd4d4a84cd520
jsp <jonsp14@gmail.com>
2020-09-29 21:19:35 -0700

    fix lowercase inputs

--- script.js
+++ script.js
@@ -57,8 +57,8 @@ $(document).ready(function () {
           break;
         }
       }
-      if (save === true) {
-        addCity(x);
+      if (save) {
+        addCity(response.name);
       }
     });
   }
